85b6e10be63481e8334196799e43d5337cf4b761
Eduard Urbach <e.urbach@gmail.com>
2018-06-24 16:14:11 +0900

    Sanitizer is no longer part of the markdown render

--- Render.go
+++ Render.go
@@ -2,16 +2,12 @@ package markdown
 
 import (
 	"github.com/aerogo/aero"
-	"github.com/microcosm-cc/bluemonday"
-	"github.com/russross/blackfriday"
+	blackfriday "gopkg.in/russross/blackfriday.v2"
 )
-
-var strictPolicy = bluemonday.StrictPolicy()
 
 // Render converts the given markdown code to an HTML string.
 func Render(code string) string {
 	codeBytes := aero.StringToBytesUnsafe(code)
-	codeBytes = strictPolicy.SanitizeBytes(codeBytes)
-	codeBytes = blackfriday.MarkdownCommon(codeBytes)
+	codeBytes = blackfriday.Run(codeBytes)
 	return aero.BytesToStringUnsafe(codeBytes)
 }
