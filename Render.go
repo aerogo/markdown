a914da2ea06e877ea4ece0d9d6ef85668874f0ca
Eduard Urbach <e.urbach@gmail.com>
2018-11-22 07:41:52 +0900

    Re-enabled bluemonday

--- Render.go
+++ Render.go
@@ -2,12 +2,16 @@ package markdown
 
 import (
 	"github.com/aerogo/aero"
+	"github.com/microcosm-cc/bluemonday"
 	blackfriday "gopkg.in/russross/blackfriday.v2"
 )
+
+var policy = bluemonday.UGCPolicy()
 
 // Render converts the given markdown code to an HTML string.
 func Render(code string) string {
 	codeBytes := aero.StringToBytesUnsafe(code)
+	codeBytes = policy.SanitizeBytes(codeBytes)
 	codeBytes = blackfriday.Run(codeBytes)
 	return aero.BytesToStringUnsafe(codeBytes)
 }
